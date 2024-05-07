59bbfcc3a851eb91eb2bc2ade44e26e1df04d476
prasidha acharya <prasidhaacharya@prasidhas-Mac-mini.local>
2024-05-07 15:52:04 +0545

    written function to fetch spell by id and set constant unique keys

--- src/api/spell.ts
+++ src/api/spell.ts
@@ -1,14 +1,52 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
+import { SpellResult } from "../model/spell";
 import { instance } from "../network";
-import { GET_SPELL_URL } from "../url/backend";
+import { SPELL_URL } from "../url/backend";
+
+const UNIQUE_KEYS = {
+  FAVOURITE: "favourite",
+  SPELL: "spell",
+} as const;
 
 // fetch all queries
 
-const getAllSpells = async () => {
-  const response = await instance.get(GET_SPELL_URL);
-  return response;
+const getAllSpells = async (): Promise<SpellResult> => {
+  const response = await instance.get(SPELL_URL);
+  return response.data;
 };
 
 export const useGetAllSpells = () => {
-  return useQuery("spell", getAllSpells);
+  return useQuery({ queryKey: [UNIQUE_KEYS.SPELL], queryFn: getAllSpells });
+};
+
+// fetch spell detail by id(index)
+
+const getSpellsByURL = async (index: string) => {
+  const SPELL_URL_BY_ID = `${SPELL_URL}/${index}`;
+
+  const response = await instance.get(SPELL_URL_BY_ID);
+  return response.data;
+};
+
+export const useGetSpellsByURL = (index: string) => {
+  return useQuery({
+    queryKey: [UNIQUE_KEYS.SPELL, index],
+    queryFn: () => getSpellsByURL(index),
+    enabled: !!index,
+  });
+};
+
+export const useGetFavouriteSpells = () => {
+  return useQuery({
+    queryKey: [UNIQUE_KEYS.FAVOURITE],
+    queryFn: () => {
+      const favouriteSpell = localStorage.getItem(UNIQUE_KEYS.FAVOURITE);
+      return JSON.parse(favouriteSpell ?? "");
+    },
+  });
+};
+
+// Add fourite ids to local storage
+export const addToFavouriteToLocalStorage = (favouriteIds: string[]) => {
+  localStorage.setItem(UNIQUE_KEYS.FAVOURITE, JSON.stringify(favouriteIds));
 };
