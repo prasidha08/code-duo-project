bb9974e346d54cd785ee8a8589e8be8b3832e661
prasidha acharya <prasidhaacharya@prasidhas-Mac-mini.local>
2024-05-07 23:04:44 +0545

    added typography customize theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,24 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({
+let theme = createTheme({
   palette: {
     primary: {
       main: "rgb(133, 179, 219)",
     },
   },
 
+  typography: {
+    h1: {
+      fontSize: 20,
+      letterSpacing: 1,
+      fontWeight: "bolder",
+    },
+    h2: {
+      fontSize: 16,
+      wordSpacing: 2,
+      letterSpacing: 1,
+    },
+  },
   components: {
     MuiButton: {
       defaultProps: {
