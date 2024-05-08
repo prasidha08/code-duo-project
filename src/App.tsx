af5b1dd2491eeb10391949203eb66590ef3e1c3c
prasidha acharya <prasidhaacharya@prasidhas-Mac-mini.local>
2024-05-08 10:13:32 +0545

    shifted Main component to new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { Box, Typography } from "@mui/material";
-import "./App.css";
+import Main from "./components/Main";
 import NavBar from "./components/NavBar";
-import ScrollableTabs, { alphateIndex } from "./components/Tabs";
-import SpellLists from "./spell/SpellLists";
-import { useState } from "react";
 
 function App() {
   return (
@@ -14,26 +10,3 @@ function App() {
   );
 }
 export default App;
-
-function Main() {
-  const [value, setValue] = useState("A");
-
-  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
-    setValue(alphateIndex[newValue]);
-  };
-
-  return (
-    <Box
-      width="80%"
-      margin="auto"
-      display="flex"
-      flexDirection="column"
-      gap={6}
-      padding={10}
-    >
-      <Typography variant="h1">Spell Card</Typography>
-      <ScrollableTabs value={value} handleChange={handleChange} />
-      <SpellLists value={value} />
-    </Box>
-  );
-}
